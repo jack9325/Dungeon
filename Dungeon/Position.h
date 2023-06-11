8eff7fb7407af62fbb33a88a7b53d7a527ddf1a3
YueBaiTW <office123580@gmail.com>
2023-06-11 16:02:09 +0800

    1

--- Dungeon/Position.h
+++ Dungeon/Position.h
@@ -1,18 +1,20 @@
 /***********************************************************************
  * File: Position.h
  * Author: B11115029 白安睿
- * Create Date: 2023/
+ * Create Date: 2023/5/28
  * Editor: B11115029 白安睿
- * Update Date: 2023/
- * Description: 
+ * Update Date: 2023/6/11
+ * Description: The class to store the coordinates of 2D vector;
  * ***********************************************************************/
 #pragma once
 #include <iostream>
 
 class Position {
 public:
+	//x and y
 	int x, y; 
 
+	//Default Constructor
 	Position();
 	Position(int x, int y);
 	Position(const Position& ref);
