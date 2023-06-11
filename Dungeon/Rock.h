0179079d09ee7248b05e465b0f503f8c0dc48d65
jack9325 <134849602+jack9325@users.noreply.github.com>
2023-06-11 18:43:57 +0800

    Update Rock.h

--- Dungeon/Rock.h
+++ Dungeon/Rock.h
@@ -1,3 +1,11 @@
+/***********************************************************************
+ * File: Trigger.h
+ * Author: B11115029白安睿 B11115046黃健嘉
+ * Create Date: 2023/5/28
+ * Editor: B11115029白安睿 B11115046黃健嘉
+ * Update Date: 2023/6/11
+ * Description: Declare a Trigger class,which has position,icon,and exp
+ * ***********************************************************************/
 #pragma once
 #include "Creature.h"
 
